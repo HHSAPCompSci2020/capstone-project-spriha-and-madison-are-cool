3bae5f7ff8f2b0e7a7b27bff6795c4bb9d0ddd4a
sprihapandey <spandey748@student.fuhsd.org>
2021-05-06 10:53:11 -0700

    Fixed compiler errors in obstacle

--- FlappyOrdeal/src/Obstacle.java
+++ FlappyOrdeal/src/Obstacle.java
@@ -2,16 +2,23 @@ import java.awt.Rectangle;
 import java.awt.Shape;
 import java.util.ArrayList;
 
+import processing.core.PImage;
+
 public class Obstacle extends MovingImage {
+
+	public Obstacle(PImage img, int x, int y, int w, int h) {
+		super(img, x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
 
 	private ArrayList<Shape> pipes;
 
 	public void draw() {
 		pipes = new ArrayList<Shape>();
-		pipe.add(new Rectangle(50, 0, 50, 100));
-		pipe.add(new Rectangle(50, 350, 50, 500));
-		pipe.add(new Rectangle(200, 0, 200, 200));
-		pipe.add(new Rectangle(200, 0, 200, 400));
+		pipes.add(new Rectangle(50, 0, 50, 100));
+		pipes.add(new Rectangle(50, 350, 50, 500));
+		pipes.add(new Rectangle(200, 0, 200, 200));
+		pipes.add(new Rectangle(200, 0, 200, 400));
 
 	}
 
