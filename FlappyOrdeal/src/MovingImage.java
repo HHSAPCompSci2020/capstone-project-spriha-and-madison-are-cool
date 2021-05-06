382757039cfa050fd9463de90b0cf3d469811472
Madison TIppett <mtippett361@student.fuhsd.org>
2021-05-06 10:47:18 -0700

    Updated obstacle and movingimage

--- FlappyOrdeal/src/MovingImage.java
+++ FlappyOrdeal/src/MovingImage.java
@@ -1,4 +1,33 @@
+import java.awt.geom.Rectangle2D;
+import processing.core.PApplet;
+import processing.core.PImage;
 
-public class MovingImage {
+public class MovingImage extends Rectangle2D.Double {
+	private PImage image;
 
+	public MovingImage(PImage img, int x, int y, int w, int h) {
+		super(x, y, w, h);
+		image = img;
+	}
+
+	public void moveToLocation(double x, double y) {
+		super.x = x;
+		super.y = y;
+	}
+
+	public void moveByAmount(double x, double y) {
+		super.x += x;
+		super.y += y;
+	}
+
+	public void applyWindowLimits(int windowWidth, int windowHeight) {
+		x = Math.min(x, windowWidth - width);
+		y = Math.min(y, windowHeight - height);
+		x = Math.max(0, x);
+		y = Math.max(0, y);
+	}
+
+	public void draw(PApplet g) {
+		g.image(image, (int) x, (int) y, (int) width, (int) height);
+	}
 }
--- FlappyOrdeal/src/Obstacle.java
+++ FlappyOrdeal/src/Obstacle.java
@@ -1,4 +1,18 @@
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.util.ArrayList;
 
 public class Obstacle extends MovingImage {
+
+	private ArrayList<Shape> pipes;
+
+	public void draw() {
+		pipes = new ArrayList<Shape>();
+		pipe.add(new Rectangle(50, 0, 50, 100));
+		pipe.add(new Rectangle(50, 350, 50, 500));
+		pipe.add(new Rectangle(200, 0, 200, 200));
+		pipe.add(new Rectangle(200, 0, 200, 400));
+
+	}
 
 }
