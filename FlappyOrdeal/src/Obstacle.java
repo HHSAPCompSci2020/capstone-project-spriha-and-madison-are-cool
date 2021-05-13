b851a2b5c663313a5ef25cf813bdec1cb516514a
sprihapandey <spandey748@student.fuhsd.org>
2021-05-13 13:30:34 -0700

    commit

--- FlappyOrdeal/src/Obstacle.java
+++ FlappyOrdeal/src/Obstacle.java
@@ -38,6 +38,7 @@ public class Obstacle extends MovingImage {
 	 * @param wiggle How much the obstacle "wiggles" (moves up and down)
 	 */
 <<<<<<< HEAD
+<<<<<<< HEAD
 	public Obstacle(double x, double y, double h) {
 		super(x, y, 80, h);
 		double topHeight = Math.random() * (0.69 * height) + 0.01 * height;
@@ -50,6 +51,8 @@ public class Obstacle extends MovingImage {
 		minShiftExtent = 50 ;
 		maxShiftExtent = (int)height - 50 ;
 =======
+=======
+>>>>>>> parent of ccc30bc (Update)
 	public Obstacle(double x, double y, double w, double h, int wiggle) {
 		super(x, y, w, h);
 
